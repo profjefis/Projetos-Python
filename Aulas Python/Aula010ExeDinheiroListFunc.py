b3410a1118d804c3b15869799153b753a9a60798
profjefis <profjefis@gmail.com>
2019-05-18 13:32:42 -0300

    Exercício Dinheiro

---
+++ Aulas Python/Aula010ExeDinheiroListFunc.py
@@ -0,0 +1,14 @@
+valorNotas = [100,50,20,10,5,2,1]
+valor = int(input('Qual o valor do dinheiro? '))
+quantNotas = [int(valor/100),int((valor%100)/50),int(((valor%100)%50)/20),int((((valor%100)%50)%20)/10),int(((((valor%100)%50)%20)%10)/5),int((((((valor%100)%50)%20)%10)%5)/2),int(((((((valor%100)%50)%20)%10)%5)%2)/1)]
+def printnotas():
+    for num in range (len(quantNotas)):
+        if quantNotas[num] == 1:
+            print(quantNotas[num],'nota de',valorNotas[num],'R$')
+        else:
+            print(quantNotas[num],'notas de',valorNotas[num],'R$')
+print('você possui:')
+printnotas()
+
+
+
